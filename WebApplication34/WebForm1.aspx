91452f25b35bd805f8f638f8335e7b807390b4f7
Jorge Castro <jorge.castro.c@gmail.com>
2019-07-29 13:46:49 -0400

    ok.

---
+++ WebApplication34/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication34.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True">
+            </asp:DropDownList>
+            <br />
+            <asp:DropDownList ID="DropDownList2" runat="server">
+            </asp:DropDownList>
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication34/WebForm1.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication34
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private string country="";
+        private string city="";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var countries = new string[] {"--seleccione un pais--", "Chile", "Argentina", "Peru" };
+            if (!IsPostBack)
+            {
+                DropDownList1.DataSource=countries;
+                DropDownList1.DataBind();
+                
+            } else
+            {
+                country=DropDownList1.SelectedValue; // guardamos el pais seleccionado
+                city=DropDownList2.SelectedValue; // guardamos la ciudad seleccionada
+
+           
+                string[] cities;
+
+                switch(country)
+                {
+                    case "Chile":
+                        cities=new string[] {"-","Arica","Iquique","Antofagasta"};
+                        break;
+                    case "Argentina":
+                        cities = new string[] { "-", "Buenos Aires", "Tigre", "Bariloche" };
+                        break;
+                    case "Peru":
+                        cities = new string[] { "-", "Lima", "Cusco", "Arequipa" };
+                        break;
+                    default:
+                            cities=new string[]{ "-", "--Nada seleccionado" };
+                            break;
+                }
+                
+                DropDownList2.DataSource=cities;
+                DropDownList2.DataBind(); // perdemos la seleccion.
+
+                if (cities.Where(c=>c== city).Count()>0) // buscamos si en ciudades esta la ciudad seleccionada. Si es asi, recuperamos la seleccion
+                {
+
+
+                    DropDownList2.SelectedValue=city; // si la seleccion es diferente a "-", seleccionamos el valor
+                }
+                
+           
+
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text="Pais: "+country+" Ciudad: "+city;
+        }
+    }
+}
